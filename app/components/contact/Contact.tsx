0ddcbd30902afd4adda6934ecfc0446f6db0a2c5
Baptiste Lejeune <baptiste.lejeune@hotmail.com>
2024-02-14 14:30:50 +0100

    🐛 FIX: style

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         <h2 className='text-center font-semibold text-3xl uppercase mb-6 md:mb-16 w-full'>Contact</h2>
         <div className='w-full md:w-[40%]'>
         <p className='text-secondary-text gap-3 items-center hidden md:flex'><GrContact style={{color: 'C4C4C4'}} /> Contactez-nous</p>
-        <p className={`my-6`}>Une question, un renseignement ? Contactez-nous au <a href="tel:+33661558029" className='font-medium'>06 61 55 80 29</a> ou via le formulaire de contact.</p>
+        <p className={`my-6`}>Une question, un renseignement ? Contactez-nous au <a href="tel:+33661558029" className='font-medium inline-block'>06 61 55 80 29</a> ou via le formulaire de contact.</p>
         </div>
         <ContactForm />
         </div>
